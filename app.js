eaab07c941716e1761f438e5eef30563d91136fe
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2024-02-14 07:28:15 +0700

    refactored

--- app.js
+++ app.js
@@ -8,22 +8,20 @@ const port = 3000;
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    try {
-        const { prompt } = req.query;
-        if (!prompt) {
-            return res
-                .status(400)
-                .json({ error: "Prompt parameter is required" });
-        }
-        const chat = await getChatInstance();
-        const result = await chat.sendMessage(prompt);
-        const response = result.response;
-        res.json(response.text());
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const { prompt } = req.query;
+    if (!prompt) {
+      return res.status(400).json({ error: "Prompt parameter is required" });
     }
+    const chat = await getChatInstance();
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    res.json(response.text());
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`App is running on port: ${port}`);
 });
