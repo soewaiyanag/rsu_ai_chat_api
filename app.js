79f53093e8904b770832aa395b0b53d9fde57a29
Soe Wai Yan Aung <soewaiyan.a@unii.co.th>
2024-03-20 10:03:17 +0700

    update safetySettings

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const PORT = 3000;
 const API_KEY = process.env.API_KEY;
-const initialHistory = require("./initialHistory");
-const express = require("express");
-const cors = require("cors");
+const initialHistory = require('./initialHistory');
+const express = require('express');
+const cors = require('cors');
 const {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
-} = require("@google/generative-ai");
-require("dotenv").config();
+} = require('@google/generative-ai');
+require('dotenv').config();
 
 const app = express();
 
@@ -24,24 +24,24 @@ const generationConfig = {
 const safetySettings = [
   {
     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
   },
 ];
 
-app.post("/chat", async (req, res) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+app.post('/chat', async (req, res) => {
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
   const history = [...initialHistory, ...req.body.history];
 
